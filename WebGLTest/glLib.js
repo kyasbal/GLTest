616685c2890d1cc15a56886d1dc1521b4545422f
LimeStreem <LimeStreem@gmail.com>
2015-03-11 17:52:30 +0900

    Add initial files

---
+++ WebGLTest/glLib.js
@@ -0,0 +1,150 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
+}
+
+var JThree;
+(function (JThree) {
+    var JsHack = (function () {
+        function JsHack() {
+        }
+        JsHack.getObjectName = function (obj) {
+            var funcNameRegex = /function (.{1,})\(/;
+            var result = (funcNameRegex).exec((obj).constructor.toString());
+            return (result && result.length > 1) ? result[1] : "";
+        };
+        return JsHack;
+    })();
+
+    var JThreeObject = (function () {
+        function JThreeObject() {
+        }
+        JThreeObject.prototype.toString = function () {
+            return JsHack.getObjectName(this);
+        };
+        return JThreeObject;
+    })();
+    JThree.JThreeObject = JThreeObject;
+
+    var DegreeMilliSecoundUnitConverter = (function (_super) {
+        __extends(DegreeMilliSecoundUnitConverter, _super);
+        function DegreeMilliSecoundUnitConverter() {
+            _super.apply(this, arguments);
+        }
+        DegreeMilliSecoundUnitConverter.prototype.toRadian = function (val) {
+            return JThreeMath.PI / 180 * val;
+        };
+
+        DegreeMilliSecoundUnitConverter.prototype.fromRadian = function (radian) {
+            return 180 / JThreeMath.PI * radian;
+        };
+
+        DegreeMilliSecoundUnitConverter.prototype.toMilliSecound = function (val) {
+            return val * 1000;
+        };
+
+        DegreeMilliSecoundUnitConverter.prototype.fromMilliSecound = function (milliSecound) {
+            return milliSecound / 1000;
+        };
+        return DegreeMilliSecoundUnitConverter;
+    })(JThreeObject);
+    JThree.DegreeMilliSecoundUnitConverter = DegreeMilliSecoundUnitConverter;
+
+    var JThreeMath = (function (_super) {
+        __extends(JThreeMath, _super);
+        function JThreeMath(unitConverter) {
+            _super.call(this);
+            this.converter = unitConverter || new DegreeMilliSecoundUnitConverter();
+        }
+        JThreeMath.prototype.radianResult = function (f) {
+            return this.converter.fromRadian(f());
+        };
+
+        JThreeMath.prototype.radianRequest = function (v, f) {
+            return f(this.converter.toRadian(v));
+        };
+
+        JThreeMath.prototype.getCurrentConverter = function () {
+            return this.converter;
+        };
+
+        /**
+        * 正弦
+        */
+        JThreeMath.prototype.sin = function (val) {
+            return this.radianRequest(val, function (val) {
+                return Math.sin(val);
+            });
+        };
+
+        /**
+        * 余弦
+        */
+        JThreeMath.prototype.cos = function (val) {
+            return this.radianRequest(val, function (val) {
+                return Math.cos(val);
+            });
+        };
+
+        /**
+        * 正接
+        */
+        JThreeMath.prototype.tan = function (val) {
+            return this.radianRequest(val, function (val) {
+                return Math.tan(val);
+            });
+        };
+
+        JThreeMath.prototype.asin = function (val) {
+            return this.radianResult(function () {
+                return Math.asin(val);
+            });
+        };
+
+        JThreeMath.prototype.acos = function (val) {
+            return this.radianResult(function () {
+                return Math.acos(val);
+            });
+        };
+
+        JThreeMath.prototype.atan = function (val) {
+            return this.radianResult(function () {
+                return Math.atan(val);
+            });
+        };
+        JThreeMath.PI = Math.PI;
+
+        JThreeMath.E = Math.E;
+        return JThreeMath;
+    })(JThreeObject);
+    JThree.JThreeMath = JThreeMath;
+
+    var Vector2 = (function (_super) {
+        __extends(Vector2, _super);
+        function Vector2() {
+            _super.apply(this, arguments);
+        }
+        Vector2.prototype.toString = function () {
+            return "Vector2(x={0},y={1})".format(this.x, this.y);
+        };
+        return Vector2;
+    })(JThreeObject);
+    JThree.Vector2 = Vector2;
+})(JThree || (JThree = {}));
+
+window.onload = function (e) {
+    var converter = new JThree.DegreeMilliSecoundUnitConverter();
+    var m = new JThree.JThreeMath(converter);
+    alert("Test{0},{1}".format(100, "Hello World"));
+};
+//# sourceMappingURL=glLib.js.map
---
+++ WebGLTest/glLib.ts
@@ -0,0 +1,190 @@
+﻿interface String {
+    format(...replacements: any[]): string;
+}
+
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    };
+}
+
+module JThree
+{
+    interface Action<T>
+    {
+        (arg:T):void;
+    }
+
+    interface Func1<T, R>
+    {
+        (arg:T):R;
+    }
+
+    interface Func0<R>
+    {
+        ():R;
+    }
+
+    class JsHack
+    {
+        public static getObjectName(obj:any): string
+        {
+            var funcNameRegex = /function (.{1,})\(/;
+            var result = (funcNameRegex).exec((obj).constructor.toString());
+            return (result && result.length > 1) ? result[1] : "";
+        }
+
+    }
+
+    export interface IUnitConverter
+    {
+        toRadian(val:number): number;
+        fromRadian(radian:number):number;
+        toMilliSecound(val:number): number;
+        fromMilliSecound(milliSecound:number):number;
+    }
+
+    export interface IStringConvertable
+    {
+        toString():string;
+    }
+
+    export class JThreeObject implements IStringConvertable
+    {
+        toString(): string
+        {
+            return JsHack.getObjectName(this);
+        }
+    }
+    
+    export class DegreeMilliSecoundUnitConverter extends JThreeObject implements IUnitConverter
+    {
+        toRadian(val: number): number
+        {
+            return JThreeMath.PI / 180 * val;
+        }
+
+        fromRadian(radian: number): number
+        {
+            return 180 / JThreeMath.PI * radian;
+        }
+
+        toMilliSecound(val: number): number
+        {
+            return val * 1000;
+        }
+
+        fromMilliSecound(milliSecound: number): number
+        {
+            return milliSecound / 1000;
+        }
+    }
+
+    export class JThreeMath extends JThreeObject
+    {
+        public static PI:number = Math.PI;
+
+        public static E: number = Math.E;
+
+        private converter: IUnitConverter;
+
+        private radianResult(f: Func0<number>): number
+        {
+            return this.converter.fromRadian(f());
+        }
+
+        private radianRequest(v:number,f: Func1<number,number>): number
+        {
+            return f(this.converter.toRadian(v));
+        }
+
+        public getCurrentConverter(): IUnitConverter
+        {
+            return this.converter;
+        }
+
+        constructor(unitConverter?: IUnitConverter)
+        {
+            super();
+            this.converter = unitConverter || new DegreeMilliSecoundUnitConverter();
+        }
+
+        /**
+         * 正弦
+         */
+        public sin(val: number):number
+        {
+            return this.radianRequest(val, (val) =>
+            {
+                return Math.sin(val);
+            });
+        }
+        /**
+         * 余弦
+         */
+        public cos(val: number): number
+        {
+            return this.radianRequest(val, (val) =>
+            {
+                return Math.cos(val);
+            });
+        }
+        /**
+         * 正接
+         */
+        public tan(val: number): number
+        {
+            return this.radianRequest(val, (val) =>
+            {
+                return Math.tan(val);
+            });
+        }
+
+        public asin(val: number): number
+        {
+            return this.radianResult(() =>
+            {
+                return Math.asin(val);
+            });
+        }
+
+        public acos(val: number): number {
+            return this.radianResult(() => {
+                return Math.acos(val);
+            });
+        }
+
+        public atan(val: number): number {
+            return this.radianResult(() => {
+                return Math.atan(val);
+            });
+        }
+
+    }
+
+    export class Vector2 extends JThreeObject
+    {
+        private x: number;
+        private y:number;
+
+
+
+        toString(): string
+        {
+            return "Vector2(x={0},y={1})".format(this.x,this.y);
+        }
+    }
+}
+
+window.onload = (e) =>
+{
+    var converter = new JThree.DegreeMilliSecoundUnitConverter();
+    var m = new JThree.JThreeMath(converter);
+    alert("Test{0},{1}".format(100,"Hello World"));
+};
