04d45e39fe3b7b4294ec7623eef78f67ab1345c3
LimeStreem <LimeStreem@gmail.com>
2015-03-21 01:48:30 +0900

    switch collection test into QUnit

--- WebGLTest/jThree.Test/CollectionTest.js
+++ WebGLTest/jThree.Test/CollectionTest.js
@@ -1,43 +1,24 @@
-var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
-};
 ///<reference path="../_references.ts"/>
 ///<reference path="../jThree/Collections.ts"/>
-var jThreeTest;
-(function (jThreeTest) {
-    var Collection;
-    (function (_Collection) {
-        var Collection = jThree.Collections.Collection;
-        var CollectionTest = (function (_super) {
-            __extends(CollectionTest, _super);
-            function CollectionTest() {
-                _super.apply(this, arguments);
-                this.testArray1 = [1, 2, 3, 4, 5];
-                this.testArray2 = [6, 7, 8, 9, 10, 11, 23];
-            }
-            CollectionTest.prototype.foreachTest = function () {
-                var result = 0;
-                var enArray1 = new jThree.Collections.ArrayEnumratorFactory(this.testArray1);
-                Collection.foreach(enArray1, function (t) {
-                    result += t;
-                });
-                this.areIdentical(result, 15);
-            };
-            CollectionTest.prototype.foreachPairTest = function () {
-                var result = 0;
-                var enArray1 = new jThree.Collections.ArrayEnumratorFactory(this.testArray1);
-                var enArray2 = new jThree.Collections.ArrayEnumratorFactory(this.testArray2);
-                Collection.foreachPair(enArray1, enArray2, function (a, b) {
-                    result += a * b;
-                });
-                this.areIdentical(result, 130);
-            };
-            return CollectionTest;
-        })(tsUnit.TestClass);
-        _Collection.CollectionTest = CollectionTest;
-    })(Collection = jThreeTest.Collection || (jThreeTest.Collection = {}));
-})(jThreeTest || (jThreeTest = {}));
+///<reference path="../Scripts/typings/qunit/qunit.d.ts"/>
+var testArray1 = [1, 2, 3, 4, 5];
+var testArray2 = [6, 7, 8, 9, 10, 11, 23];
+QUnit.module("Collection test");
+QUnit.test("foreach test", function () {
+    var result = 0;
+    var enArray1 = new jThree.Collections.ArrayEnumratorFactory(testArray1);
+    jThree.Collections.Collection.foreach(enArray1, function (t) {
+        result += t;
+    });
+    QUnit.equal(result, 15);
+});
+QUnit.test("foreach pair test", function () {
+    var result = 0;
+    var enArray1 = new jThree.Collections.ArrayEnumratorFactory(testArray1);
+    var enArray2 = new jThree.Collections.ArrayEnumratorFactory(testArray2);
+    jThree.Collections.Collection.foreachPair(enArray1, enArray2, function (a, b) {
+        result += a * b;
+    });
+    QUnit.equal(result, 130);
+});
 //# sourceMappingURL=CollectionTest.js.map
--- WebGLTest/jThree.Test/CollectionTest.ts
+++ WebGLTest/jThree.Test/CollectionTest.ts
@@ -1,26 +1,21 @@
 ﻿///<reference path="../_references.ts"/>
 ///<reference path="../jThree/Collections.ts"/>
-module jThreeTest.Collection {
-    import Collection = jThree.Collections.Collection;
-    import Enumrator = jThree.Collections.IEnumrator;
-
-    export class CollectionTest extends tsUnit.TestClass {
-        testArray1: number[] = [1, 2, 3, 4, 5];
-        testArray2: number[]=[6, 7, 8, 9, 10,11,23];
-
-        foreachTest() {
+///<reference path="../Scripts/typings/qunit/qunit.d.ts"/>
+        var testArray1: number[] = [1, 2, 3, 4, 5];
+        var testArray2: number[]=[6, 7, 8, 9, 10,11,23];
+        QUnit.module("Collection test");
+        QUnit.test("foreach test", () => {
             var result: number = 0;
-            var enArray1: jThree.Collections.IEnumerable<number> = new jThree.Collections.ArrayEnumratorFactory(this.testArray1);
-            Collection.foreach(enArray1, (t) => { result += t });
-            this.areIdentical(result, 15);
-        }
+            var enArray1: jThree.Collections.IEnumerable<number> = new jThree.Collections.ArrayEnumratorFactory(testArray1);
+            jThree.Collections.Collection.foreach(enArray1, (t) => { result += t });
+            QUnit.equal(result, 15);
+        });
 
-        foreachPairTest() {
+        QUnit.test("foreach pair test", () => {
             var result: number = 0;
-            var enArray1: jThree.Collections.IEnumerable<number> = new jThree.Collections.ArrayEnumratorFactory(this.testArray1);
-            var enArray2: jThree.Collections.IEnumerable<number> = new jThree.Collections.ArrayEnumratorFactory(this.testArray2);
-            Collection.foreachPair(enArray1,enArray2,(a,b) => { result += a*b});
-            this.areIdentical(result, 130);
-        }
-    }
-}
+            var enArray1: jThree.Collections.IEnumerable<number> = new jThree.Collections.ArrayEnumratorFactory(testArray1);
+            var enArray2: jThree.Collections.IEnumerable<number> = new jThree.Collections.ArrayEnumratorFactory(testArray2);
+            jThree.Collections.Collection.foreachPair(enArray1, enArray2, (a, b) => { result += a * b });
+            QUnit.equal(result, 130);
+        });
+    
